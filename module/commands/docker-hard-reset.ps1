f301b24a56e70bae9ae3f0e0f73b684463393a23
kkowalski1 <krzysztofk@ntropy.pl>
2026-03-03 11:39:16 +0100

    docker-hard-reset fixes

--- module/commands/docker-hard-reset.ps1
+++ module/commands/docker-hard-reset.ps1
@@ -25,37 +25,96 @@ function start-docker-4-win {
     return $true;
 }
 
-start-docker-4-win;
+try {
+    start-docker-4-win;
 
-Write-Host "Stopping docker";
-docker stop $(docker ps -q)
-Write-Host "Removing docker images";
-docker rm -f $(docker ps -aq)
+    Write-Host "Stopping docker";
+    docker stop $(docker ps -q)
+    Write-Host "Removing docker images";
+    docker rm -f $(docker ps -aq)
 
-Write-Host "Removing docker volumes";
-docker system prune -a --volumes -f
+    Write-Host "Removing docker volumes";
+    docker system prune -a --volumes -f
 
-sleep(1);
+    sleep(1);
 
-docker system prune -a --volumes -f
+    docker system prune -a --volumes -f
 
-sleep(1);
+    sleep(1);
 
-docker volume rm $(docker volume ls -q)
+    docker volume rm $(docker volume ls -q)
 
 
-Write-Host "Recreate docker containers";
-$proc = Start-Process powershell -ArgumentList "-File", "start-docker.ps1" -PassThru
+    Write-Host "Recreate docker containers";
+    $proc = Start-Process powershell  `
+        -ArgumentList "-File", "$PSScriptRoot\start-docker.ps1" `
+        -PassThru
 
-Write-Host "Pull newest changes";
-$oldDir = $pwd;
-cd c:\dev\thp\thunderpick
-git pull
+    Write-Host "Pull newest changes";
+    $oldDir = $pwd;
+    cd c:\dev\thp\thunderpick
+    git pull
 
-Write-Host "Waiting for docker compose";
-$proc.WaitForExit();
 
-dotnet ef database drop --force --project Thunderpick.Data --startup-project Thunderpick.Api
-dotnet ef database update --verbose --project Thunderpick.Data --startup-project Thunderpick.Api
+    # Step 1: Get required SDK version from global.json
+    if (!(Test-Path "global.json")) {
+        Write-Host "No global.json found. Running dotnet ef directly..."
+        dotnet ef @args
+    } else {
+        $globalJson = Get-Content "global.json" | ConvertFrom-Json
+        $requiredVersion = $globalJson.sdk.version
 
-cd $oldDir;
+        Write-Host "Required SDK version: $requiredVersion"
+        $installDir = "$env:USERPROFILE\.dotnet"
+        $env:PATH = "$installDir;$env:PATH"
+        
+        # Step 2: Check installed SDKs
+        $installedSdks = dotnet --list-sdks | ForEach-Object {
+            ($_ -split '\s+')[0]
+        }
+
+        if ($installedSdks -contains $requiredVersion) {
+            Write-Host "SDK $requiredVersion is already installed."
+        }
+        else {
+            Write-Host "SDK $requiredVersion NOT installed. Installing..."
+            Get-Process dotnet -ErrorAction SilentlyContinue | ForEach-Object {
+                Write-Host "Stopping dotnet process Id=$($_.Id)"
+                Stop-Process -Id $_.Id -Force
+            }
+
+            Start-Sleep -Seconds 2
+
+            # Download dotnet-install.ps1 if not exists
+            if (!(Test-Path ".\dotnet-install.ps1")) {
+                Invoke-WebRequest `
+                    -Uri https://dot.net/v1/dotnet-install.ps1 `
+                    -OutFile dotnet-install.ps1
+            }
+
+            .\dotnet-install.ps1 `
+                -Version $requiredVersion `
+                -InstallDir $installDir
+
+            # Install required SDK
+            .\dotnet-install.ps1 -Version $requiredVersion -InstallDir $installDir
+
+            Write-Host "Installation completed."
+        }
+    }
+
+    Write-Host "Waiting for docker compose";
+    $proc.WaitForExit();
+
+    dotnet ef database drop --force --project Thunderpick.Data --startup-project Thunderpick.Api
+    dotnet ef database update --verbose --project Thunderpick.Data --startup-project Thunderpick.Api
+
+    cd $oldDir;
+}
+catch {
+    Write-Error $_
+    Write-Host ""
+    Write-Host "An error occurred. Press Enter to close..."
+    Read-Host
+    exit 1
+}
--- module/commands/start-docker.ps1
+++ module/commands/start-docker.ps1
@@ -1,36 +1,50 @@
-function start-docker-4-win {
-    docker ps *> $null
-    if(!($?)) {
-        write-host 'Starting docker...'
-        C:\'Program Files'\Docker\Docker\'Docker Desktop.exe'
+Start-Transcript -Path "C:\Tools\automation\start-docker.log" -Append
 
-        $counter = 20;
-        while ($true) {
-            docker ps *> $null
-            if ($?) {
-                break;
+try {
+    function start-docker-4-win {
+        docker ps *> $null
+        if(!($?)) {
+            write-host 'Starting docker...'
+            C:\'Program Files'\Docker\Docker\'Docker Desktop.exe'
+
+            $counter = 20;
+            while ($true) {
+                docker ps *> $null
+                if ($?) {
+                    break;
+                }
+                $counter--;
+                
+                if ($counter -le 0) {
+                    break;
+                }
+                Start-Sleep -Seconds 1
+                Write-Host '.'
             }
-            $counter--;
             
-            if ($counter -le 0) {
-                break;
-            }
-            Start-Sleep -Seconds 1
-            Write-Host '.'
+            return ($counter -gt 0);
         }
         
-        return ($counter -gt 0);
+        return $true;
     }
-    
-    return $true;
+
+    start-docker-4-win;
+
+    cd C:\dev\thp\thunderpick\dev-setup\docker-compose-stacks\infrastructure-stacks
+    docker compose up -d
+    cd ..\static-stack
+    docker compose up -d
+    docker stop thp-nginx
+    docker stop thp-api
+    docker start thp-nginx
 }
-
-start-docker-4-win;
-
-cd C:\dev\thp\thunderpick\dev-setup\docker-compose-stacks\infrastructure-stacks
-docker compose up -d
-cd ..\static-stack
-docker compose up -d
-docker stop thp-nginx
-docker stop thp-api
-docker start thp-nginx
+catch {
+    Write-Error $_
+    Write-Host ""
+    Write-Host "An error occurred. Press Enter to close..."
+    Read-Host
+    exit 1
+}
+finally {
+    Stop-Transcript
+}
